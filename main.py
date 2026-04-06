c7f1e5b97810802ef51f7be9025fa6aebf9df789
nbonofiglio <nicholas.bonofiglio@gmail.com>
2026-04-06 13:56:52 -0400

    added foundation for conversations

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     import argparse
     from dotenv import load_dotenv
     from google import genai
+    from google.genai import types
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,7 +16,9 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt text being sent to Gemini")
     args = parser.parse_args()
 
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
     
