3d933e1ce6fea3900bf74ff4730914af9f636729
nbonofiglio <nicholas.bonofiglio@gmail.com>
2026-04-06 14:20:40 -0400

    added verbose flag

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
 
     parser = argparse.ArgumentParser(description="Prompt Gemini")
     parser.add_argument("user_prompt", type=str, help="User prompt text being sent to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -21,13 +22,16 @@ def main():
     response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-    
+
     if not prompt_tokens:
         raise RuntimeError("Invalid prompt token count")
     if not response_tokens:
         raise RuntimeError("Invalid response token count")
     
-    print(f"Prompt tokens: ${prompt_tokens}\nResponse tokens: ${response_tokens}\nResponse: ${response.text}")
+    if args.verbose:
+        print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}\nUser prompt: {args.user_prompt}\nResponse: {response.text}")
+    else:
+        print(f"Response: {response.text}")
 
 if __name__ == "__main__":
     main()
