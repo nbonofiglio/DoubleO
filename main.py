9dfc5cb50f88944c90a9450657d3f216269fe646
nbonofiglio <nicholas.bonofiglio@gmail.com>
2026-04-06 13:18:31 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    import os
+    from dotenv import load_dotenv
+    from google import genai
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if (api_key is None):
+        raise RuntimeError("missing Gemini API key")
+    
+    client = client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
