b9f2b0e554f7d3296725cfa620e316861b70c75f
nbonofiglio <nicholas.bonofiglio@gmail.com>
2026-04-06 13:42:02 -0400

    add token counts output

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ def main():
     client = client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    
+    if not prompt_tokens:
+        raise RuntimeError("Invalid prompt token count")
+    if not response_tokens:
+        raise RuntimeError("Invalid response token count")
+    
+    print(f"Prompt tokens: ${prompt_tokens}\nResponse tokens: ${response_tokens}\nResponse: ${response.text}")
 
 if __name__ == "__main__":
     main()
