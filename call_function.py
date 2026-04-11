240cf8c66044f4e206114ef655964e858a74e6b7
nbonofiglio <nicholas.bonofiglio@gmail.com>
2026-04-10 23:08:40 -0400

    connected agent ability to read, write, and run

--- call_function.py
+++ call_function.py
@@ -1,15 +1,52 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
 
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info, schema_get_file_content, schema_run_python_file, schema_write_file],
 )
-"""
-For each declaration, you'll need to update at minimum the name and description of the function, as well as the key and description of each item in parameters.properties.
-If any function arguments are required, it's a good idea to put them in a required list under parameters. For example, the declaration for get_file_content would have required=["file_path"].
-If a function accepts more than one argument from the LLM caller, the declaration will have multiple items in parameters.properties. For example, the declaration for write_file should include both file_path and content.
-Pay attention to the type of each parameter. Most of them will be types.Type.STRING, but the optional args parameter of run_python_file should be types.Type.ARRAY – which then has items that are types.Type.STRING. Yes, this can get pretty nested!
-"""
+
+function_map = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file
+}
+
+def call_function(function_call, verbose=False):
+    function_name = function_call.name or ""
+
+    if (function_name not in function_map):
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    
+    if (verbose):
+        print(f"Calling function: {function_name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+    
+    args = dict(function_call.args) if function_call.args else {}
+    args["working_directory"] = "./calculator"
+    
+    result = function_map[function_name](**args)
+    
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": result},
+            )
+    ],
+)
+
+    
--- main.py
+++ main.py
@@ -5,14 +5,14 @@ def main():
     from google import genai
     from google.genai import types
     from prompt import system_prompt
-    from call_function import available_functions
+    from call_function import available_functions, call_function
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if (api_key is None):
         raise RuntimeError("missing Gemini API key")
     
-    client = client = genai.Client(api_key=api_key)
+    client = genai.Client(api_key=api_key)
 
     parser = argparse.ArgumentParser(description="Prompt Gemini")
     parser.add_argument("user_prompt", type=str, help="User prompt text being sent to Gemini")
@@ -24,27 +24,37 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0),
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
         )
     
     functions = response.function_calls
 
-    if (functions != None):
-        for function in functions:
-            print(f"Calling function: {function.name}({function.args})")
-    else:
-        prompt_tokens = response.usage_metadata.prompt_token_count
-        response_tokens = response.usage_metadata.candidates_token_count
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
 
-        if not prompt_tokens:
-            raise RuntimeError("Invalid prompt token count")
-        if not response_tokens:
-            raise RuntimeError("Invalid response token count")
+    if not prompt_tokens:
+        raise RuntimeError("Invalid prompt token count")
+    if not response_tokens:
+        raise RuntimeError("Invalid response token count")
+
+    if (functions == None):
+        print(f"-> {response.text}")
+        return
+    
+    results = []
+    for function in functions:
+        function_call_result = call_function(function, verbose=args.verbose)
+        if (len(function_call_result.parts) == 0):
+            raise Exception(f"Result of {function} is empty and should not be")
+        if (function_call_result.parts[0].function_response is None):
+            raise Exception(f"Response for {function} has no value")
+        if (function_call_result.parts[0].function_response.response is None):
+            raise Exception("Response attribute of Response object has not value")
         
-        if args.verbose:
-            print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}\nUser prompt: {args.user_prompt}\nResponse: {response.text}")
-        else:
-            print(f"Response: {response.text}")
+        results.append(function_call_result.parts[0])
+
+        if (args.verbose):
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 
 if __name__ == "__main__":
     main()
