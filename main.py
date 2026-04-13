d27e0d10721c4eae5037f1ef37165a4a0c0526d3
nbonofiglio <nicholas.bonofiglio@gmail.com>
2026-04-13 15:32:03 -0400

    added agent ability to iterate

--- main.py
+++ main.py
@@ -21,40 +21,50 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
+    for i in range(20):
+ 
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
         )
+
+        for candidate in response.candidates:
+            messages.append(candidate.content)
     
-    functions = response.function_calls
+        functions = response.function_calls
 
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
 
-    if not prompt_tokens:
-        raise RuntimeError("Invalid prompt token count")
-    if not response_tokens:
-        raise RuntimeError("Invalid response token count")
+        if not prompt_tokens:
+            raise RuntimeError("Invalid prompt token count")
+        if not response_tokens:
+            raise RuntimeError("Invalid response token count")
 
-    if (functions == None):
-        print(f"-> {response.text}")
-        return
-    
-    results = []
-    for function in functions:
-        function_call_result = call_function(function, verbose=args.verbose)
-        if (len(function_call_result.parts) == 0):
-            raise Exception(f"Result of {function} is empty and should not be")
-        if (function_call_result.parts[0].function_response is None):
-            raise Exception(f"Response for {function} has no value")
-        if (function_call_result.parts[0].function_response.response is None):
-            raise Exception("Response attribute of Response object has not value")
-        
-        results.append(function_call_result.parts[0])
+        if (functions == None):
+            print(f"-> {response.text}")
+            break
 
-        if (args.verbose):
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+        results = []
+   
+        for function in functions:
+            function_call_result = call_function(function, verbose=args.verbose)
+            if (len(function_call_result.parts) == 0):
+                raise Exception(f"Result of {function} is empty and should not be")
+            if (function_call_result.parts[0].function_response is None):
+                raise Exception(f"Response for {function} has no value")
+            if (function_call_result.parts[0].function_response.response is None):
+                raise Exception("Response attribute of Response object has not value")
+            results.append(function_call_result.parts[0])
+            if (args.verbose):
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        messages.append(types.Content(role="user", parts=results))
+
+    else:
+        print("Iteration limit reached")
+        exit(1)
 
 if __name__ == "__main__":
     main()
